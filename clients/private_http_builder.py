a413643a80f6c22c1f8a911edf9f1361f820730a
raf-zig <ziravin#yandex.ru>
2025-08-28 20:45:46 +0500

    Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from httpx import Client
 from pydantic import BaseModel
 
@@ -7,11 +9,11 @@ from clients.authentication.authentication_schema import LoginRequestSchema
 
 
 # Добавили суффикс Schema вместо Dict
-class AuthenticationUserSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
+class AuthenticationUserSchema(BaseModel, frozen=True):  # Наследуем от BaseModel вместо TypedDict
     email: str
     password: str
 
-
+@lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
 
