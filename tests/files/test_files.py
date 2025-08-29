14f4e31b24f955278886954761856d6aa99f6817
raf-zig <ziravin#yandex.ru>
2025-08-29 11:05:37 +0500

    Практикуемся в написании API автотестов. Часть 1. Практика написания негативных API автотестов для работы с файлами.

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -86,7 +86,7 @@ class TestFiles:
         validate_json_schema(response.json(), response_data.model_json_schema())
 
     def test_get_file_with_incorrect_file_id(self, files_client: FilesClient):
-        response = files_client.get_file_api("incorrect-file-id")
+        response = files_client.get_file_api(file_id="incorrect-file-id")
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
         assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
         assert_get_file_with_incorrect_file_id_response(response_data)
