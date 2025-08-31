fbf7e6066ab19794a95da0ddfabf3b46515f9460
raf-zig <ziravin#yandex.ru>
2025-08-31 21:15:31 +0500

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение списка заданий

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -5,12 +5,13 @@ import pytest
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
+    ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema, \
+    GetExercisesQuerySchema, GetExercisesResponseSchema
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response, assert_exercise_not_found_response
+    assert_update_exercise_response, assert_exercise_not_found_response, assert_get_exercises_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -51,4 +52,13 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.NOT_FOUND)
         response_data = InternalErrorResponseSchema.model_validate_json(response.text)
         assert_exercise_not_found_response(response_data)
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercises(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture, function_course: CourseFixture):
+        request = GetExercisesQuerySchema(course_id=function_course.response.course.id)
+        response = exercises_client.get_exercises_api(request)
+        response_data = GetExercisesResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercises_response([function_exercise.response], get_exercises_response=response_data)
         validate_json_schema(response.json(), response_data.model_json_schema())
